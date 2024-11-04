585462ad5f543cf3fc1252d1f08f4dbe298bc2b3
Geraint <luffgd@gmail.com>
2024-11-04 09:04:59 +0000

    Pause `<audio>` when engine stopped

--- clap-host/wasi/wasi_snapshot_preview1.mjs
+++ clap-host/wasi/wasi_snapshot_preview1.mjs
@@ -101,6 +101,8 @@ function wasi_snapshot_preview1(args=[], env={}, fileResolver) {
 	'args_get,args_sizes_get,clock_res_get,clock_time_get,environ_get,environ_sizes_get,fd_advise,fd_allocate,fd_close,fd_datasync,fd_fdstat_get,fd_fdstat_set_flags,fd_fdstat_set_rights,fd_filestat_get,fd_filestat_set_size,fd_filestat_set_times,fd_pread,fd_prestat_get,fd_prestat_dir_name,fd_pwrite,fd_read,fd_readdir,fd_renumber,fd_seek,fd_sync,fd_tell,fd_write,path_create_directory,path_filestat_get,path_filestat_set_times,path_link,path_open,path_readlink,path_remove_directory,path_rename,path_symlink,path_unlink_file,poll_oneoff,proc_exit,proc_raise,random_get,sched_yield,sock_recv,sock_send,sock_shutdown'.split(',').forEach(key => {
 		if (!wasi[key]) {
 			wasi[key] = (...args) => {
+				console.error(`Missing WASI: ${key}(${args.join(', ')})`);
+				debugger;
 				throw Error('WASI: ' + key + '(' + args.join(', ') + ')');
 				return 0;
 			};
